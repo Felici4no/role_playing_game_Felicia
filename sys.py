b9e6daa0e9c3049ed9bfa5591f263fa1aafd431b
Lucas Feliciano <lucas.for.study.42@gmail.com>
2024-03-31 20:24:31 -0300

    criação de classe

---
+++ sys.py
@@ -0,0 +1,9 @@
+print('...._....')
+
+class Main:
+    def __init__(self,name,at,de,vl,sl1):
+        self.name = name
+        self.at = at
+        self.de = de
+        self.vl = vl
+        sl1.sl1 = sl1
